a0aa1101863125215d7f9176374fb706505be835
Abdullah Çok <abdullahcok@abdullahcok.com>
2019-01-07 20:59:00 +0300

    components' files

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+    	<div>
+        		<h1 className="title-container__title">Weafind</h1>
+        		<h3 className="title-container__subtitle">Find out temperature where you want to...</h3>
+    	</div>
+);
+
+export default Titles;
